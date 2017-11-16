bfd2a5f367d5a7b029bed9225d11ff2c15818a03
Jon Moroney <j.moroney@cs.ru.nl>
2017-11-16 15:38:31 +0100

    Need newer rust

--- ddh/src/main.rs
+++ ddh/src/main.rs
@@ -1,25 +1,37 @@
 use std::fs;
+use blake2::{Blake2b, Digest};
 
 fn main() {
-    println!("Hello, world!");
     let first_dir = String::from(".");
-    read_dir(first_dir);
+    recurse_on_dir(first_dir);
 }
 
-fn read_dir(current_dir: String) -> std::io::Result<()>{
-    let mut files: Vec<std::fs::File> = Vec::new();
+fn recurse_on_dir(current_dir: String) -> std::io::Result<()>{
+    let mut files: Vec<String> = Vec::new();
     let mut sub_directories: Vec<std::fs::DirEntry> = Vec::new();
 
     //Read files and directories
     for entry in fs::read_dir(current_dir)? {
         let item = entry?;
-        println!("{:?}", item.path());
-        sub_directories.push(item);
+        if (item.file_type()?.is_file()){
+            //println!("{:?} is a file", item.path());
+            files.push(item.file_name().into_string().unwrap());
+        } else{
+            //println!("{:?} is a dir", item.path());
+            sub_directories.push(item);
+        }
     }
 
-    for entry in sub_directories::drain().collect()? {
-        let item = entry?;
-        println!("{:?}", item.path());
+    //Print current files and hashes
+    for entry in files.iter() {
+        let the_file = std::fs::File::open(entry);
+
+        //let item = entry?;
+        println!("{:?} is a file", entry);
+    }
+
+    for entry in sub_directories.iter(){
+        println!("{:?} is a dir", entry.path());
     }
     Ok(())
 }

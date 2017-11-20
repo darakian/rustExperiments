bbe92f9c5c0a0003036871f581376c6c03f55e7b
Jon Moroney <darakian@gmail.com>
2017-11-20 20:38:59 +0100

    Passing hash

--- ddh/src/main.rs
+++ ddh/src/main.rs
@@ -6,6 +6,8 @@ use std::fs::{self, DirEntry};
 extern crate sha2;
 use sha2::{Sha256, Digest};
 
+extern crate generic_array;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     //Check Args length
@@ -17,6 +19,9 @@ fn main() {
     } else if args.len() == 2 {
         let first_path = Path::new(&args[1]);
         let directory_vector = recurse_on_dir(first_path);
+        for entry in directory_vector.unwrap().iter(){
+            println!("{:x}\t > {}", entry.1, entry.0);
+        }
     } else if args.len() == 3 {
         let first_path = Path::new(&args[1]);
         let second_path = Path::new(&args[2]);
@@ -32,9 +37,9 @@ fn main() {
     //recurse_on_dir(first_dir);
 }
 
-fn recurse_on_dir(current_dir: &Path) -> Result<Vec<String>, io::Error>{
-    println!("Entering directory: {:?}", current_dir.to_str().unwrap());
-    let mut files: Vec<String> = Vec::new();
+fn recurse_on_dir(current_dir: &Path) -> Result<Vec<(String, generic_array::GenericArray<u8, generic_array::typenum::U32>)>, io::Error>{
+    //println!("Entering directory: {:?}", current_dir.to_str().unwrap());
+    let mut files: Vec<(String, generic_array::GenericArray<u8, generic_array::typenum::U32>)> = Vec::new();
     let mut sub_directories: Vec<Box<Path>> = Vec::new();
 
     //Read files and directories
@@ -43,8 +48,7 @@ fn recurse_on_dir(current_dir: &Path) -> Result<Vec<String>, io::Error>{
         if item.file_type()?.is_file(){
             let mut file = fs::File::open(item.path())?;
             let hash = Sha256::digest_reader(&mut file)?;
-            println!("{:x}\t{}", hash, item.file_name().into_string().unwrap());
-            files.push(item.file_name().into_string().unwrap());
+            files.push((item.file_name().into_string().unwrap(), hash));
         } else{
             sub_directories.push(item.path().into_boxed_path());
         }
@@ -52,7 +56,7 @@ fn recurse_on_dir(current_dir: &Path) -> Result<Vec<String>, io::Error>{
 
     for sub_dir in sub_directories.iter(){
         let additional_files = recurse_on_dir(&*sub_dir)?;
-        files.extend_from_slice(&additional_files);
+        files.extend(additional_files.iter().cloned());
     }
 
     return Ok(files)

0c45d8e22a97cb50358322f6c2605ea8488b60a8
Jon Moroney <darakian@gmail.com>
2017-12-04 17:21:23 +0100

    Changed intersection => shared. Cleaned up printing

--- ddh/src/main.rs
+++ ddh/src/main.rs
@@ -96,10 +96,10 @@ fn main() {
     let common_files: HashSet<Fileinfo> = directory_results.iter().fold(complete_files.clone(), |intersection_of_elements, element| intersection_of_elements.intersection(element).cloned().collect());
     println!("{:?} Total number of files: {:?} {}", complete_files.iter().fold(0, |sum, x| sum+x.file_paths.len()), complete_files.iter().fold(0, |sum, x| sum+(x.file_len*x.file_paths.len() as u64))/display_divisor, blocksize);
     println!("{:?} Total unique files: {:?} {}", complete_files.len(), complete_files.iter().fold(0, |sum, x| sum+x.file_len)/display_divisor, blocksize);
-    println!("{:?} Files in the intersection: {:?} {}", common_files.len(), common_files.iter().fold(0, |sum, x| sum+x.file_len)/display_divisor, blocksize);
+    println!("{:?} Shared files: {:?} {}", common_files.len(), common_files.iter().fold(0, |sum, x| sum+x.file_len)/display_divisor, blocksize);
     match arguments.value_of("Print").unwrap_or(""){
-        "U" => {println!("Unique Files"); complete_files.iter().for_each(|x| if(x.file_paths.len())==1{println!("{:?}", x.file_paths.iter().for_each(|y| println!("{:?}", y)))});},
-        "S" => {println!("Shared Files"); common_files.iter().for_each(|x| println!("{:?}", x.file_paths[0]));},
+        "U" => {println!("Unique Files"); complete_files.iter().for_each(|x| if(x.file_paths.len())==1{x.file_paths.iter().for_each(|y| println!("{}", y.to_str().unwrap()))});},
+        "S" => {println!("Shared Files"); common_files.iter().for_each(|x| println!("{:?}", x.file_paths[0].to_str().unwrap()));},
         _ => {}};
     //println!("{:?} Files in the symmetric difference: {:?} {}", unique_files.len(), (unique_files.iter().fold(0, |sum, x| sum+x.file_len))/display_divisor, blocksize);
 }

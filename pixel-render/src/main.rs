1284e64e20043f3f65d5d2c1be50acdc9508d9b5
Jon Moroney <darakian@gmail.com>
2018-01-31 12:38:50 +0100

    Update based on piston example code

--- pixel-render/src/main.rs
+++ pixel-render/src/main.rs
@@ -1,8 +1,22 @@
-extern crate image;
+extern crate piston_window;
 
-use image::{GenericImage, ImageBuffer};
+use piston_window::*;
 
 fn main() {
-    let display_buffer = ImageBuffer::new(256, 240);
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Hello Pixels!", [640, 480])
+        .exit_on_esc(true).build().unwrap();
+
+    let mut x_draw_pos = 0.0;
+    let mut y_draw_pos = 0.0;
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([1.0; 4], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [x_draw_pos, y_draw_pos, 100.0, 100.0],
+                      c.transform, g);
+        });
+        x_draw_pos=(x_draw_pos+15.0)%640.0;
+        y_draw_pos=(y_draw_pos+5.0)%480.0;
+    }
 }

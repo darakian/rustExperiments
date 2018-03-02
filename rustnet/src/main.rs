fff8182106b291f4f93e3b92cd2ec1126ce1826b
Jon Moroney <darakian@gmail.com>
2018-03-02 10:01:49 +0100

    Now sending crap to google

--- rustnet/src/main.rs
+++ rustnet/src/main.rs
@@ -2,6 +2,6 @@
 use std::net::UdpSocket;
 
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:3401").expect("couldn't bind to address");
-    socket.send_to(&[0; 10], "128.171.159.1:4242").expect("couldn't send data");
+    let socket = UdpSocket::bind("0.0.0.0:3401").expect("couldn't bind to address");
+    socket.send_to(&[0; 10], "8.8.8.8:4242").expect("couldn't send data");
 }

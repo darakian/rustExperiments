c2adee392173d08e627769864bce6f0892740b70
Jon Moroney <darakian@gmail.com>
2018-03-23 14:24:15 +0100

    Update plotting

--- file_compare/plotter.sh
+++ file_compare/plotter.sh
@@ -4,4 +4,4 @@ do
   echo "${i}Bytes"
   gnuplot -e "datafile='data/${i}Bytes'; outputname='plots/${i}Bytes.png'" plotter.plg
 done
-convert -loop 0 -delay 500 plots/*.png out.gif
+convert -loop 0 -delay 125 plots/*.png out.gif

528a0d9f9c4ec4b1909d29ad8652319dbbac6fa9
Jon Moroney <darakian@gmail.com>
2018-04-02 15:22:51 +0200

    Done with this for now

--- file_compare/plotter.sh
+++ file_compare/plotter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+./target/release/file_compare $1
 for i in $(ls data)
 do
-  gnuplot -e "datafile='data/${i}'; outputfile='plots/${i}Bytes.png'" plotter.plg
+  gnuplot -e "datafile='data/${i}'; outputfile='plots/${i}Bytes.png'; bytes='${i}'" plotter.plg
 done
-convert -loop 0 -delay 125 plots/*.png out.gif
+convert -loop 0 -delay 100 plots/*.png out2.gif
--- file_compare/src/main.rs
+++ file_compare/src/main.rs
@@ -93,8 +93,8 @@ fn main() {
         x.1
     ).flatten().collect();
 
-    for i in 1..1000{
-        let mut file = File::create(format!("data/{:05}",i*10)).unwrap();
+    for i in 1..100{
+        let mut file = File::create(format!("data/{:05}",i)).unwrap();
         //write!(file, "Step {}\n", i).unwrap();
         complete_files.par_iter_mut().for_each(|x| hash_and_update(x, i));
         complete_files.par_sort_unstable_by(|a, b| b.file_hash.cmp(&a.file_hash));
@@ -113,8 +113,8 @@ fn hash_and_update(input: &mut Fileinfo, length: u64) -> (){
     match fs::File::open(input.file_paths.iter().next().expect("Error opening file for hashing")) {
         Ok(f) => {
             let mut buffer_reader = BufReader::new(f);
-            let mut hash_buffer = [0;100];
-            for _i in 1..length {
+            let mut hash_buffer = [0;1];
+            for _i in 0..length {
                 match buffer_reader.read(&mut hash_buffer) {
                     Ok(n) if n>0 => hasher.write(&hash_buffer[0..n]),
                     Ok(n) if n==0 => break,

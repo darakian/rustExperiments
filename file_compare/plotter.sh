e289210c1d39989220b42620817f6aee4169017e
Jon <jmoroney@hawaii.edu>
2018-05-10 16:38:54 -1000

    Make sure dirs are in place

--- file_compare/plotter.sh
+++ file_compare/plotter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#./target/release/file_compare $1
+./target/release/file_compare $1
+mkdir -p data
+mkdir -p plots
 for i in $(ls data)
 do
   gnuplot -e "datafile='data/${i}'; outputfile='plots/${i}Bytes.png'; bytes='${i}'" plotter1.plg
@@ -7,3 +9,6 @@ do
 done
 gnuplot -e "datafile='consolidated.txt'; outputfile='histogram.png';" plotter2.plg
 convert -loop 0 -delay 100 plots/*.png unique.gif
+
+#cleanup
+rm consolidated.txt

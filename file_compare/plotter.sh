d2dd1800a5f84dd68d4438fb2e2a15fbc06833df
Jon Moroney <darakian@gmail.com>
2018-05-12 10:42:26 -1000

    Comment out gif creation. Memory requirements are just too high

--- file_compare/plotter.sh
+++ file_compare/plotter.sh
@@ -4,13 +4,13 @@ mkdir -p data
 mkdir -p plots
 for i in $(ls data)
 do
-  gnuplot -e "datafile='data/${i}'; outputfile='plots/${i}Bytes.png'; bytes='${i}'" plotter1.plg
+  #gnuplot -e "datafile='data/${i}'; outputfile='plots/${i}Bytes.png'; bytes='${i}'" plotter1.plg
   wc -l data/${i} | sed 's/data\//''/g' >> consolidated.txt
 done
 nl filesizes > numbered_filesizes
 gnuplot -e "datafile='consolidated.txt'; outputfile='histogram.png';" plotter2.plg
 gnuplot -e "datafile='numbered_filesizes'; outputfile='sizes.png';" plotter3.plg
-nice convert -loop 0 -delay 100 plots/*.png -fuzz 10% -layers Optimize unique.gif
+#nice convert -loop 0 -delay 100 plots/*.png -fuzz 10% -layers Optimize unique.gif
 
 #cleanup
 rm consolidated.txt

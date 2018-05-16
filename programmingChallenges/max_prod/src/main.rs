24079bd84db517554d9bbfe0c011f7bdbd7f001c
Jon Moroney <darakian@gmail.com>
2018-05-16 10:05:22 -1000

    Add quickcheck to ensure equality

--- programmingChallenges/max_prod/src/main.rs
+++ programmingChallenges/max_prod/src/main.rs
@@ -1,9 +1,23 @@
+extern crate quickcheck;
+use quickcheck::{TestResult, quickcheck};
+
+
 fn main() {
-    println!("{}", max_product_naive(vec![5,7,-1,2,10]));
-    println!("{}", max_product_better(vec![5,7,-1,2,10]));
+    let the_vec = vec![5,7,1,22,10,100, 0, 0, 0, -999];
+    println!("{}", max_product_naive(the_vec.clone()));
+    println!("{}", max_product_better(the_vec.clone()));
+
+        fn prop(xs: Vec<i64>) -> TestResult {
+            if xs.len() == 0 {
+              return TestResult::discard()
+              }
+            TestResult::from_bool(max_product_naive(xs.clone()) == max_product_better(xs.clone()))
+        }
+        quickcheck(prop as fn(Vec<i64>) -> TestResult);
 }
 
 fn max_product_naive(input: Vec<i64>) -> i64{
+    if input.len()<3{return -1}
     let mut products: Vec<(i64, i64, i64, i64)> = Vec::new();
 
     for (i_index, i_element) in input.iter().enumerate(){
@@ -18,9 +32,9 @@ fn max_product_naive(input: Vec<i64>) -> i64{
     return products.iter().max_by(|a, b| a.3.cmp(&b.3)).unwrap().3
 }
 
-fn max_product_better(input: Vec<i64>) -> i64{
-    let mut products: Vec<(i64, i64, i64, i64)> = Vec::new();
-
-
-    return products.iter().max_by(|a, b| a.3.cmp(&b.3)).unwrap().3
+fn max_product_better(mut input: Vec<i64>) -> i64{
+    if input.len()<3{return -1}
+    input.sort();
+    //println!("{:?}", input);
+    return std::cmp::max(input[0]*input[1]*input[input.len()-1], input[input.len()-1]*input[input.len()-2]*input[input.len()-3])
 }

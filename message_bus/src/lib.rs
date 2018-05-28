815fefbc424e53b5dc08351ebceab3a06a314ebe
Jon Moroney <darakian@gmail.com>
2018-05-27 17:20:40 -1000

    Change subscribers to hashmap

--- message_bus/src/lib.rs
+++ message_bus/src/lib.rs
@@ -13,6 +13,7 @@ extern crate crossbeam_channel;
 use self::crossbeam_channel::unbounded;
 
 use std::sync::Mutex;
+use std::collections::hash_map::{HashMap, Entry};
 
     struct Message{
         to_id: u32,
@@ -22,18 +23,26 @@ use std::sync::Mutex;
     struct Messagebus{
         bus_id: u64,
         m_buffer: Mutex<Vec<Message>>,
-        subscribers: Vec<(u32, crossbeam_channel::Sender<Message>, crossbeam_channel::Receiver<Message>)>
+        subscribers:  Mutex<HashMap<u32, (crossbeam_channel::Sender<Message>, crossbeam_channel::Receiver<Message>)>>
     }
 
     impl Messagebus{
         fn new(bus_id: u64) -> Self{
-            Messagebus{bus_id: bus_id, m_buffer: Mutex::new(Vec::new()), subscribers: Vec::new()}
+            Messagebus{bus_id: bus_id, m_buffer: Mutex::new(Vec::new()), subscribers: Mutex::new(HashMap::new())}
         }
 
-        pub fn subscribe(&mut self, sub_tag: u32) -> (crossbeam_channel::Sender<Message>, crossbeam_channel::Receiver<Message>){
+        pub fn subscribe(&mut self, sub_tag: u32) -> Result<(crossbeam_channel::Sender<Message>, crossbeam_channel::Receiver<Message>), &str>{
             let (send, receive) = unbounded::<Message>();
-            self.subscribers.push((sub_tag, send.clone(), receive.clone()));
-            (send, receive)
+            match self.subscribers.get_mut(){
+                Ok(exclusive_buffer) => {
+                    match exclusive_buffer.entry(sub_tag) {
+                        Entry::Vacant(eb) => {eb.insert((send.clone(), receive.clone()));},
+                        Entry::Occupied(mut e) => {return Err("Sub_ID in use");}
+                        }
+                },
+                Err(e) => {println!("{:?}", e);return Err("Poison error")}
+            }
+            Ok((send, receive))
         }
 
         fn process(&mut self) {
@@ -45,14 +54,11 @@ use std::sync::Mutex;
                 },
                 Err(e) => {}
             }
-
         }
 
         pub fn publish(&mut self, m: Message) -> (){
             match self.m_buffer.get_mut(){
-                Ok(exclusive_buffer) => {
-                    exclusive_buffer.push(m)
-                },
+                Ok(exclusive_buffer) => exclusive_buffer.push(m),
                 Err(e) => {}
             }
         }

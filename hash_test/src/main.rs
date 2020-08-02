2b311ec933f91c49fbbbb41294e81432e044e4d6
Jon Moroney <darakian@gmail.com>
2020-08-02 11:28:59 -0700

    Variable run length

--- hash_test/src/main.rs
+++ hash_test/src/main.rs
@@ -3,79 +3,93 @@ use std::fs::File;
 use std::io::Read;
 use std::hash::Hasher;
 use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+
 use blake3;
 use seahash;
-
-const HASH_ITERATIONS: u32 = 100;
 
 #[derive(Debug)]
 struct Args {
     help: bool,
+    iters: Option<u32>,
     file: Option<String>,
 }
 
 fn main() {
+    let mut hash_iterations: u32 = 100;
     let mut args = pico_args::Arguments::from_env();
     // Arguments can be parsed in any order.
     let args = Args {
         help: args.contains(["-h", "--help"]),
+        iters: args.opt_value_from_str(["-i", "--iters"]).unwrap_or(None),
         file: args.opt_value_from_str(["-f", "--file"]).unwrap_or(None),
     };
+    match args.iters{
+        Some(i) => hash_iterations = i,
+        None => {},
+    }
+    let mut results = HashMap::new();
 
     match args.file {
         Some(f) => {
             println!("Hashing {:?}", f);
-            bench_sip_hash(f.clone());
-            bench_sea_hash(f.clone());
-            bench_blake3_hash(f.clone());
+            results.insert("Sip",
+                bench_sip_hash(f.clone(), hash_iterations));
+            results.insert("Sea",
+                bench_sea_hash(f.clone(), hash_iterations));
+            results.insert("Blake3",
+                bench_blake3_hash(f.clone(), hash_iterations));
             },
         None => {
             println!("No file");
             return},
     }
+    for token in results.iter(){
+        println!("{:?}", token);
+    }
 }
 
-fn bench_sip_hash(file: String) {
+fn bench_sip_hash(file: String, hash_iterations: u32) -> Vec<Duration> {
+    let mut results = Vec::with_capacity(hash_iterations as usize);
     let mut file = File::open(file).expect("Error opening file");
     let mut buffer: Vec<u8> = Vec::new();
     file.read_to_end(&mut buffer).expect("Error reading file");
-    let mut total_time = Duration::new(0, 0);
-    for _i in 0..HASH_ITERATIONS{
+    for _i in 0..hash_iterations{
         let mut sip_hasher = DefaultHasher::new();
-        let sip_now = Instant::now();
+        let now = Instant::now();
         sip_hasher.write(&buffer);
         sip_hasher.finish();
-        total_time += sip_now.elapsed();
+        results.push(now.elapsed());
     }
-    println!("Sip Hash took {:?} on average", total_time/HASH_ITERATIONS);
+    results
 }
 
-fn bench_sea_hash(file: String) {
+fn bench_sea_hash(file: String, hash_iterations: u32) -> Vec<Duration> {
+    let mut results = Vec::with_capacity(hash_iterations as usize);
     let mut file = File::open(file).expect("Error opening file");
     let mut buffer: Vec<u8> = Vec::new();
     file.read_to_end(&mut buffer).expect("Error reading file");
-    let mut total_time = Duration::new(0, 0);
-    for _i in 0..HASH_ITERATIONS{
+    for _i in 0..hash_iterations{
         let mut hasher = seahash::SeaHasher::new();
         let now = Instant::now();
         hasher.write(&buffer);
         hasher.finish();
-        total_time += now.elapsed();
+        results.push(now.elapsed());
     }
-    println!("Sea Hash took {:?} on average", total_time/HASH_ITERATIONS);
+    results
 }
 
-fn bench_blake3_hash(file: String) {
+fn bench_blake3_hash(file: String, hash_iterations: u32) -> Vec<Duration> {
+    let mut results = Vec::with_capacity(hash_iterations as usize);
     let mut file = File::open(file).expect("Error opening file");
     let mut buffer: Vec<u8> = Vec::new();
     file.read_to_end(&mut buffer).expect("Error reading file");
-    let mut total_time = Duration::new(0, 0);
-    for _i in 0..HASH_ITERATIONS{
+    for _i in 0..hash_iterations{
         let mut hasher = blake3::Hasher::new();
         let now = Instant::now();
         hasher.update(&buffer);
         hasher.finalize();
-        total_time += now.elapsed();
+        results.push(now.elapsed());
     }
-    println!("Blake3 took {:?} on average", total_time/HASH_ITERATIONS);
+    results
 }
